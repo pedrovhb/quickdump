fcacd727f8f46c658f1161cf312ee87949f8f515
Pedro Batista <pedrovhb@gmail.com>
2022-03-26 18:32:00 -0300

    Add TCP & HTTP servers

--- quickdump/server_http.py
+++ quickdump/server_http.py
@@ -6,10 +6,10 @@ from typing import Any
 import uvicorn
 from multidict import CIMultiDict
 from starlette.applications import Starlette
+from starlette.endpoints import HTTPEndpoint
 from starlette.requests import Request
 from starlette.responses import Response
 from starlette.routing import Route
-from starlette.endpoints import HTTPEndpoint
 
 from quickdump import QuickDumper
 from quickdump.const import _default_label
@@ -31,10 +31,7 @@ class DumpApp(HTTPEndpoint):
     async def dispatch(self) -> None:
         request = Request(self.scope, receive=self.receive)
 
-        label = request.path_params.get("label") or _default_label
-        print(request.path_params)
-        print(request.url)
-
+        label = request.path_params.get("full_path") or _default_label
         dumper = QuickDumper(label)
 
         body = await request.body()
@@ -42,7 +39,7 @@ class DumpApp(HTTPEndpoint):
         url = request.url
 
         req_dump = RequestDump(headers=headers, url=str(url), body=body)
-        dumper.dump(req_dump)
+        dumper.dump(req_dump, force_flush=True)
 
         response = Response(status_code=201)
         await response(self.scope, self.receive, self.send)
@@ -52,7 +49,6 @@ app = Starlette(
     debug=True,
     routes=[
         Route("/{full_path:path}", DumpApp),
-        # Route("/", DumpApp),
     ],
 )
 
